ec9cc08ccc3f6bc3cf9d16cc61a0f52777a7e180
Ethan Pineda <joshuapineda66@gmail.com>
2024-09-20 19:46:54 -0500

    don't need post router

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "115e8144e2473bdf"; }
+/******/ 		__webpack_require__.h = function() { return "b139771955b5bde4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
